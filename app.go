a0320b3d5da401cdca0ff8594ac349a8a41ec985
gorkem acar <54164989+gor2em@users.noreply.github.com>
2023-11-02 01:01:05 +0300

    Update app.go

--- app.go
+++ app.go
@@ -230,7 +230,7 @@ func main() {
 
 
 	// set zone with soa query ?soa=
-	r.Methods("POST").Path("/dns/{zoneName}/set-zone/").HandlerFunc(DoDNSZoneSet)
+	r.Methods("POST").Path("/dns/{zoneName}/set-zone/{dnsType}/").HandlerFunc(DoDNSZoneSet)
 	
 	r.Methods("POST").Path("/dns/{zoneName}/{dnsType}/{nodeName}/set/{Address}").HandlerFunc(DoDNSSet)
 	r.Methods("POST").Path("/dns/{zoneName}/{dnsType}/{nodeName}/edit/{Address}").HandlerFunc(EditDNSSet)
